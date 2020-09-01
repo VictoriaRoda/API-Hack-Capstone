4da73ce31ce9557a522955386e91e64e2fdae0c9
Victoria <62159005+VictoriaRoda@users.noreply.github.com>
2020-09-01 13:28:10 -0400

    Results Changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//random button//
+//random button function to return a random recipe//
 $('#search').click(function getRandomDrink() {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
   console.log(url);
@@ -10,29 +10,88 @@ $('#search').click(function getRandomDrink() {
     .catch(error => alert('Something went wrong. Try again later.'));
 })
 
-//search by name//
+//search by name function to format paramaters//
 function formatQueryParams(params) {
   let queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 
+//display search total//
+function searchTotal(responseJson){
+  $("#search-total").empty();
+  let totalHtml=`<p>${responseJson.drinks.length} total recipes</p>`;
+  $("#search-total").append(totalHtml);
+}
+
+
+//diplay results of the search//
 function displayResults(responseJson) {
+	let ingredientList = '';
+	let separator = '';
+	let space = ' ';
+	
   console.log(responseJson);
   $('#results-list').empty();
   for (let i = 0; i < responseJson.drinks.length; i++){
+	if(responseJson.drinks[i].strMeasure1 !=null){
+		ingredientList += responseJson.drinks[i].strMeasure1;
+	}
+	if($(responseJson.drinks[i].strIngredient1) !=null){
+		ingredientList += space + responseJson.drinks[i].strIngredient1;
+		separator = ', '
+	}
+	if(responseJson.drinks[i].strMeasure2 !=null){
+		ingredientList += separator + responseJson.drinks[i].strMeasure2;
+		if(responseJson.drinks[i].strIngredient2 !=null){
+		ingredientList += space + responseJson.drinks[i].strIngredient2;
+		separator = ', '
+	    }
+	} else {
+	  if(responseJson.drinks[i].strIngredient2 !=null){
+		ingredientList += separator + responseJson.drinks[i].strIngredient2;
+		separator = ', '
+	   }
+	}
+	if(responseJson.drinks[i].strMeasure3 !=null){
+		ingredientList += separator + responseJson.drinks[i].strMeasure3;
+		if(responseJson.drinks[i].strIngredient3 !=null){
+		ingredientList += space + responseJson.drinks[i].strIngredient3;
+		separator = ', '
+	    }
+	} else {
+	  if(responseJson.drinks[i].strIngredient3 !=null){
+		ingredientList += separator + responseJson.drinks[i].strIngredient3;
+		separator = ', '
+	   }
+	}
+	if(responseJson.drinks[i].strMeasure4 !=null){
+		ingredientList += separator + responseJson.drinks[i].strMeasure4;
+		if(responseJson.drinks[i].strIngredient4 !=null){
+		ingredientList += space + responseJson.drinks[i].strIngredient4;
+		separator = ', '
+	    }
+	} else {
+	  if(responseJson.drinks[i].strIngredient4 !=null){
+		ingredientList += separator + responseJson.drinks[i].strIngredient4;
+		separator = ', '
+	   }
+	}
     $('#results-list').append(
     `<li><h3>${responseJson.drinks[i].strDrink}</h3>
     <img src="${responseJson.drinks[i].strDrinkThumb}/preview">
     <p>Category: ${responseJson.drinks[i].strIBA}</p>
     <p>Ingredients:
-    ${responseJson.drinks[i].strMeasure1} ${responseJson.drinks[i].strIngredient1},  ${responseJson.drinks[i].strMeasure2} ${responseJson.drinks[i].strIngredient2},  ${responseJson.drinks[i].strMeasure3} ${responseJson.drinks[i].strIngredient3},  ${responseJson.drinks[i].strMeasure4} ${responseJson.drinks[i].strIngredient4}</li>
+    ${ingredientList}</li>
     <li>${responseJson.drinks[i].strInstructions}</p>
     </li>`
     )};
   $('#results').removeClass('hidden');
+  $('main').html('results');
+  searchTotal(responseJson);
 };
 
+//paramaters for the api//
 function getDrinkByName(name) {
   let params={
     "s": name,
@@ -40,6 +99,7 @@ function getDrinkByName(name) {
   getDrink(params);
 }
 
+//fetch the api function//
 function getDrink(params) {
   let searchURL='https://www.thecocktaildb.com/api/json/v1/1/search.php?';
   let queryString = formatQueryParams(params);
@@ -60,6 +120,7 @@ function getDrink(params) {
     });
 }
 
+//validate search terms//
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
